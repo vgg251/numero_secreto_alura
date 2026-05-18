bf9145c8be4dc42231ec87b6df43091113e34a05
Vitor Gabriel <vg.gabriel2139@gmail.com>
2026-05-18 13:48:23 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('Bem vindo!');
+let NumeroSecreto= parseInt(Math.random()*(mutiplicador=500)+1);
+console.log(NumeroSecreto);
+let chute;
+let tentativa=1;
+
+while (chute!=NumeroSecreto) {
+chute=prompt(`Escolha um número entre 1 e ${mutiplicador}`);
+if (chute===NumeroSecreto) {} else {
+if (chute>NumeroSecreto) {
+alert(`O número é menor que ${chute}`);} 
+else {
+alert(`O número é maior que ${chute}`);
+}
+tentativa++;
+}
+}
+
+let PalavraTentativa= tentativa>1? 'tentativas' : 'tentativa';
+alert(`Parabéns! você acertou com ${tentativa} ${PalavraTentativa}, o número é ${NumeroSecreto}`);
+
